f1a1c9a78cdec27555da734383d0f010a71a0b1d
Sanket Singh <singhsanket143@gmail.com>
2023-12-09 12:03:10 +0530

    Added codes

---
+++ Week_3/Math_library.js
@@ -0,0 +1,32 @@
+// abs
+// abs(positive_value) -> positive_value
+// abs(negative_value) -> positive_counterpart_of that negatuve value
+// |x| --> Ex: |10| -> 10, |-8| -> 8
+
+console.log(Math.abs(-9))
+console.log(Math.abs(8))
+
+// Round off -> 
+
+console.log(Math.round(4.87))
+console.log(Math.round(4.17))
+console.log(Math.round(4.5))
+
+
+// floor -> it returns first integer just less than or equal to x
+console.log(Math.floor(4.3))
+console.log(Math.floor(7.9))
+
+
+// ceil -> This helps you find, first integer greater than or equal to x
+console.log(Math.ceil(7.2))
+
+
+// Max and min
+console.log(Math.max(8, 9, 10))
+console.log(Math.min(8, 9, 10))
+
+// sqrt -> square root
+console.log(Math.sqrt(100))
+
+// 
